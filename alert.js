51e35553555f8f9d71a86f4877b28cd100d10861
miguel <miguelrusso2840@gmail.com>
2024-10-27 13:22:33 -0300

    guardo actualizacion

---
+++ alert.js
@@ -0,0 +1,2 @@
+alert('bienvenidos gamers es hora de disfrutar de los mejores juegos en la nube');
+console.log(alert);
---
+++ comentarios.js
@@ -0,0 +1,15 @@
+addEventListener = document.write('comentarios');
+console.log('comentarios');
+
+document.querySelectorAll('emails') = AuthenticatorResponse;
+
+AuthenticatorResponse.write('name') = localStorage
+
+console.log('emails')
+
+localStorage.querySelectorAll('comentarios' + 'emails');
+
+console.log(localStorage);
+
+
+
