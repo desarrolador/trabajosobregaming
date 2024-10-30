f5d4d1a43a351ba9e190bd11552496e496994c0d
miguel <miguelrusso2840@gmail.com>
2024-10-30 14:29:13 -0300

    guardo actualizacion

--- comentarios.js
+++ comentarios.js
@@ -1,15 +1,4 @@
-addEventListener = document.write('comentarios');
-console.log('comentarios');
 
-document.querySelectorAll('emails') = AuthenticatorResponse;
-
-AuthenticatorResponse.write('name') = localStorage
-
-console.log('emails')
-
-localStorage.querySelectorAll('comentarios' + 'emails');
-
-console.log(localStorage);
 
 
 
