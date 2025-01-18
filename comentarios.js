1dbe3b6f08dfb832662cf7a3051bcbb1e138cf1f
miguel <miguelrusso2840@gmail.com>
2025-01-18 13:26:54 -0300

    guardo actualizacion

--- comentarios.js
+++ comentarios.js
@@ -1,3 +1,9 @@
+document.getElementById('commentForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const comment = document.getElementById('comment').value;
+    // Aquí puedes agregar el código para enviar el comentario a tu base de datos
+    console.log('Comentario enviado:', comment);
+});
 
 
 
